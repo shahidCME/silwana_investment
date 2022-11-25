21452162a242b5fb47fa69e5b2eed2764f60d101
shahidCME <sahid.cmexpertise@gmail.com>
2022-11-25 12:23:05 +0530

    make table structured proper

--- resources/views/admin/investment/list.blade.php
+++ resources/views/admin/investment/list.blade.php
@@ -1,12 +1,13 @@
-<div class="card-box mb-30">
+<div class="card-box  investment-card-box  mb-30 ">
     <div class="pd-20">
         <h4 class="text-blue h4">{{ $title }}</>
         @if(admin_login()['role'] != '3')
         <a class="btn btn-primary pull-right" href="{{ $addButton }}" role="button">Add</a>
         @endif    
     </div>
-    <div class="pb-20">
-        <table class="table stripe hover nowrap" id="InvestmentTable">
+    <div class="pb-20 investment-card-table">
+        
+        <table class="table stripe hover nowrap " id="InvestmentTable">
             <thead>
                 <tr>
                     <th class="table-plus datatable-nosort">Customer Fullname</th>
