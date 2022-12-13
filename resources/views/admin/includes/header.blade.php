d7e706299dc81f081df7197848a6abfed2c30756
shahidCME <sahid.cmexpertise@gmail.com>
2022-12-13 20:28:56 +0530

    icon update

--- resources/views/admin/includes/header.blade.php
+++ resources/views/admin/includes/header.blade.php
@@ -12,13 +12,13 @@
 			rel="icon"
 			type="image/png"
 			sizes="32x32"
-			href="{{ asset('assets/admin/') }}/vendors/images/favicon-32x32.png"
+			href="{{ asset('assets/admin/') }}/vendors/images/favicon_new.png"
 		/>
 		<link
 			rel="icon"
 			type="image/png"
 			sizes="16x16"
-			href="{{ asset('assets/admin/') }}/vendors/images/favicon-16x16.png"
+			href="{{ asset('assets/admin/') }}/vendors/images/favicon_new.png"
 		/>
 
 		<!-- Mobile Specific Metas -->
