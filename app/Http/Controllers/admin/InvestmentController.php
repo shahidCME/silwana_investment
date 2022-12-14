089342800ec3213e956950c1029cf993e3a4becb
shahidCME <sahid.cmexpertise@gmail.com>
2022-12-14 12:48:44 +0530

    Update InvestmentController.php

--- app/Http/Controllers/admin/InvestmentController.php
+++ app/Http/Controllers/admin/InvestmentController.php
@@ -400,10 +400,7 @@ class InvestmentController extends Controller
                     // $this->insertNotification($insertData);
                     
                     // for user
-                    Elq();
                     $device = Device::where(['user_id'=>$viewData[0]->user_id,'role'=>'2'])->get();
-                    Plq();
-                    dd($device);
                     if(!empty($device->all())){
                         $plan = Schema::where('id',$viewData[0]->schema_id)->get();
                         $planName = $plan[0]->name;
