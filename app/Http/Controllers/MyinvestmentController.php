86cf74fab9616c84e1ac8a9a692583b288eb34ef
shahidCME <sahid.cmexpertise@gmail.com>
2022-12-30 16:35:26 +0530

    formated

--- app/Http/Controllers/MyinvestmentController.php
+++ app/Http/Controllers/MyinvestmentController.php
@@ -7,115 +7,115 @@ use DB;
 use DataTables;
 class MyinvestmentController extends Controller
 {
-    public function index()
-    {
-        $data['page'] = "users.investment_list";
-        $data['js'] = ['user_investment'];
-        $data['title'] = "My Investment";
-        return view('admin.main_layout',$data);
-    }
-
-    public function getUserInvestmentDataTable(Request $request){
-        if ($request->ajax()) {
-            $Session = Session::get('admin_login');
-            $query = DB:: table('investments as i');
-              $query->leftJoin('users as u', 'u.id', '=', 'i.user_id')
-                    ->leftJoin('schemas as s', 's.id', '=', 'i.schema_id')
-                    ->leftJoin('admins as a', 'a.id', '=', 'i.admin_id')
-                    ->select('i.*', 'u.fname as customerFname','u.lname as customerLname', 's.name as schema','a.fname','a.lname');
-            if($Session['role'] == '2'){
-                $id = $Session['id'];
-                $query->where('i.user_id',$id);
-            }
-            // Elq();
-            $query->where('i.status','!=','9');
-            $query->where('i.deleted_at',null);
-            $data = $query->get();
-            $viewData = $query->get();
-            return Datatables::of($data)->addIndexColumn()
-            ->addColumn('customer fullname',function($row){
-                return $row->customerFname .' '.$row->customerLname;
-            })
-            ->addColumn('sales person',function($row){
-                return $row->fname.' '.$row->lname;
-            })
-            ->addColumn('start date',function($row){
-                return date('d F Y',strtotime($row->start_date));
-            })
-            ->addColumn('end date',function($row){
-                return date('d F Y',strtotime($row->contract_end_date));
-            })
-            ->addColumn('return type',function($row){
-                return ($row->return_type =='0') ? "Monthly" : "Yearly";
-            })
-             ->addColumn('action', function($row){
-                    $encryptedId = encrypt($row->id);
-                    $view = "investmentDetails/".$encryptedId;
-                    $roi = "userRoi/".$encryptedId;
-                    $btn = '<a class="btn btn-warning btn-sm" href="'.url($roi).'"><i class="dw dw-eye"></i> ROI</a>
-                          <a class="btn btn-success btn-sm" href="'.url($view).'"><i class="dw dw-file"></i> View</a>'; 
-                        //  return $btn;
-                    if($row->contract_pdf != ''){
-                        $d_url = url('uploads/contract_pdf/'.$row->contract_pdf);
-                            $btn .='<a href="'.$d_url.'" class="btn btn-primary btn-sm" style="margin-left: 3px;" download=""><i class="fa fa-download"></i></a>';
-                    }
-                    return $btn;
-                })
-                ->addColumn('status', function($row){
-                    if($row->status == '0') {
-                        $sttus ='<button type="button" class="badge badge-danger">Rejected</button>' ;
-                    }elseif($row->status == '1'){
-                        $sttus ='<button type="button" class="badge badge-success">Approved</button>';
-                    }else{
-                        $sttus ='<button type="button" class="badge badge-warning">Pending</button>';
-                    } 
-                    return $sttus;
-                })
-                ->rawColumns(['return type','start date','end date','sales person','customer name','status','action'])
-                ->make(true);
+        public function index()
+        {
+            $data['page'] = "users.investment_list";
+            $data['js'] = ['user_investment'];
+            $data['title'] = "My Investment";
+            return view('admin.main_layout',$data);
         }
 
-        return view('admin.main_layout');
-    }
-
-    public function investmentDetails($eid){
-        $id = decrypt($eid);
-        $query = DB:: table('investments as i');
+        public function getUserInvestmentDataTable(Request $request){
+            if ($request->ajax()) {
+                $Session = Session::get('admin_login');
+                $query = DB:: table('investments as i');
                 $query->leftJoin('users as u', 'u.id', '=', 'i.user_id')
-                    ->leftJoin('schemas as s', 's.id', '=', 'i.schema_id')
-                    ->leftJoin('admins as a', 'a.id', '=', 'i.admin_id')
-                    ->select('i.*', 'u.fname as customerFname','u.lname as customerLname', 's.name as schema','a.fname','a.lname','s.details','s.documents');
-                    $query->where('i.id',$id);
-                    $query->where('i.deleted_at',null);
-                    $viewData = $query->get();
-                    foreach ($viewData as $key => $value) {
-                        if($value->return_type == '0'){
-                            $start_date = strtotime($value->start_date);
-                            $year = $value->tenure;
-                            $end_date = date('Y-m-d', strtotime("+".$year.' month',$start_date));
-                            $value->contract_end_date = $end_date;
-                        }else{
-                            $start_date = date('Y-m-d',strtotime($value->start_date));
-                            $contract_end_date = date('Y-m-d', strtotime("+".$value->tenure." year",strtotime($start_date)));
-                            $value->contract_end_date = $contract_end_date;
+                        ->leftJoin('schemas as s', 's.id', '=', 'i.schema_id')
+                        ->leftJoin('admins as a', 'a.id', '=', 'i.admin_id')
+                        ->select('i.*', 'u.fname as customerFname','u.lname as customerLname', 's.name as schema','a.fname','a.lname');
+                if($Session['role'] == '2'){
+                    $id = $Session['id'];
+                    $query->where('i.user_id',$id);
+                }
+                // Elq();
+                $query->where('i.status','!=','9');
+                $query->where('i.deleted_at',null);
+                $data = $query->get();
+                $viewData = $query->get();
+                return Datatables::of($data)->addIndexColumn()
+                ->addColumn('customer fullname',function($row){
+                    return $row->customerFname .' '.$row->customerLname;
+                })
+                ->addColumn('sales person',function($row){
+                    return $row->fname.' '.$row->lname;
+                })
+                ->addColumn('start date',function($row){
+                    return date('d F Y',strtotime($row->start_date));
+                })
+                ->addColumn('end date',function($row){
+                    return date('d F Y',strtotime($row->contract_end_date));
+                })
+                ->addColumn('return type',function($row){
+                    return ($row->return_type =='0') ? "Monthly" : "Yearly";
+                })
+                ->addColumn('action', function($row){
+                        $encryptedId = encrypt($row->id);
+                        $view = "investmentDetails/".$encryptedId;
+                        $roi = "userRoi/".$encryptedId;
+                        $btn = '<a class="btn btn-warning btn-sm" href="'.url($roi).'"><i class="dw dw-eye"></i> ROI</a>
+                            <a class="btn btn-success btn-sm" href="'.url($view).'"><i class="dw dw-file"></i> View</a>'; 
+                            //  return $btn;
+                        if($row->contract_pdf != ''){
+                            $d_url = url('uploads/contract_pdf/'.$row->contract_pdf);
+                                $btn .='<a href="'.$d_url.'" class="btn btn-primary btn-sm" style="margin-left: 3px;" download=""><i class="fa fa-download"></i></a>';
                         }
-                    }
-                    // dd($viewData);
-        $data['viewData'] = $viewData;
-        $data['page'] = 'users.investmentDetails';
-        return view('admin.main_layout',$data);
+                        return $btn;
+                    })
+                    ->addColumn('status', function($row){
+                        if($row->status == '0') {
+                            $sttus ='<button type="button" class="badge badge-danger">Rejected</button>' ;
+                        }elseif($row->status == '1'){
+                            $sttus ='<button type="button" class="badge badge-success">Approved</button>';
+                        }else{
+                            $sttus ='<button type="button" class="badge badge-warning">Pending</button>';
+                        } 
+                        return $sttus;
+                    })
+                    ->rawColumns(['return type','start date','end date','sales person','customer name','status','action'])
+                    ->make(true);
+            }
 
-    }
+            return view('admin.main_layout');
+        }
+
+        public function investmentDetails($eid){
+            $id = decrypt($eid);
+            $query = DB:: table('investments as i');
+                    $query->leftJoin('users as u', 'u.id', '=', 'i.user_id')
+                        ->leftJoin('schemas as s', 's.id', '=', 'i.schema_id')
+                        ->leftJoin('admins as a', 'a.id', '=', 'i.admin_id')
+                        ->select('i.*', 'u.fname as customerFname','u.lname as customerLname', 's.name as schema','a.fname','a.lname','s.details','s.documents');
+                        $query->where('i.id',$id);
+                        $query->where('i.deleted_at',null);
+                        $viewData = $query->get();
+                        foreach ($viewData as $key => $value) {
+                            if($value->return_type == '0'){
+                                $start_date = strtotime($value->start_date);
+                                $year = $value->tenure;
+                                $end_date = date('Y-m-d', strtotime("+".$year.' month',$start_date));
+                                $value->contract_end_date = $end_date;
+                            }else{
+                                $start_date = date('Y-m-d',strtotime($value->start_date));
+                                $contract_end_date = date('Y-m-d', strtotime("+".$value->tenure." year",strtotime($start_date)));
+                                $value->contract_end_date = $contract_end_date;
+                            }
+                        }
+                        // dd($viewData);
+            $data['viewData'] = $viewData;
+            $data['page'] = 'users.investmentDetails';
+            return view('admin.main_layout',$data);
+
+        }
 
 
-    public function userRoi($eid){
-        $id = decrypt($eid);
-        $roi = DB::table('roi')->where('investment_id',$id)->get();
-        $data['page'] = 'users.user_roi';
-        $data['js'] = []; 
-        $data['roi'] = $roi; 
-        $data['title'] = 'Return of investment'; 
-        $data['investment_id'] = '123'; 
-        return view('admin.main_layout',$data);
-    }
+        public function userRoi($eid){
+            $id = decrypt($eid);
+            $roi = DB::table('roi')->where('investment_id',$id)->get();
+            $data['page'] = 'users.user_roi';
+            $data['js'] = []; 
+            $data['roi'] = $roi; 
+            $data['title'] = 'Return of investment'; 
+            $data['investment_id'] = '123'; 
+            return view('admin.main_layout',$data);
+        }
 }
--- app/Http/Controllers/api/DashboardController.php
+++ app/Http/Controllers/api/DashboardController.php
@@ -12,97 +12,97 @@ use App\Models\admin\Schema;
 use DB;
 class DashboardController extends Controller
 {
-    public function index(Request $request){
-        $validator = Validator::make($request->all(), [
-            'role'    => 'required'
-        ]);
-        if ($validator->fails()) {
-            $responce = [
-                'status'=>'0',
-                'errors'=>$validator->errors()
-            ];
-            return response()->json($responce);
-        }
-
-        if($request->role == '1' || $request->role == '0' || $request->role == '3' || $request->role == '4' || $request->role == '2'){
-
-            if($request->role == '1' || $request->role == '0' || $request->role == '3' || $request->role == '4'){
-                $data['totalInvestment'] = Investment::sum('amount');
-                $data['totalUser'] = User::count();
-                $data['totalSalePerson'] = Admin::where('role','0')->count();
-                $data['totalFinancePerson'] = Admin::where('role','3')->count();
-                $data['totalApprover'] = Admin::where('role','4')->count();
-                $data['totalSchema'] = Schema::count();
-                $data['cancelledInvestment'] = Investment::where('status','9')->count();;
-
-            }else{
-                $validator = Validator::make($request->all(), [
-                    'role'    => 'required|numeric',
-                    'user_id' => 'required|numeric'
-                ]);
-                if ($validator->fails()) {
-                    $responce = [
-                        'status'=>'0',
-                        'errors'=>$validator->errors()
-                    ];
-                    return response()->json($responce);
-                }
-                $user_id = $request->user_id;
-                $data['totalInvestment'] = Investment::where('user_id',$user_id)->sum('amount');
-                $data['numberOfInvestment'] = Investment::where('user_id',$user_id)->count();
-                $data['totalReturn'] = DB::table('investments as i')
-                ->join('roi as r', 'i.id', '=', 'r.investment_id')
-                ->where(['i.user_id'=>$user_id,'r.status'=>'1'])->sum('r.return_amount');
+        public function index(Request $request){
+            $validator = Validator::make($request->all(), [
+                'role'    => 'required'
+            ]);
+            if ($validator->fails()) {
+                $responce = [
+                    'status'=>'0',
+                    'errors'=>$validator->errors()
+                ];
+                return response()->json($responce);
             }
-            
+
+            if($request->role == '1' || $request->role == '0' || $request->role == '3' || $request->role == '4' || $request->role == '2'){
+
+                if($request->role == '1' || $request->role == '0' || $request->role == '3' || $request->role == '4'){
+                    $data['totalInvestment'] = Investment::sum('amount');
+                    $data['totalUser'] = User::count();
+                    $data['totalSalePerson'] = Admin::where('role','0')->count();
+                    $data['totalFinancePerson'] = Admin::where('role','3')->count();
+                    $data['totalApprover'] = Admin::where('role','4')->count();
+                    $data['totalSchema'] = Schema::count();
+                    $data['cancelledInvestment'] = Investment::where('status','9')->count();;
+
+                }else{
+                    $validator = Validator::make($request->all(), [
+                        'role'    => 'required|numeric',
+                        'user_id' => 'required|numeric'
+                    ]);
+                    if ($validator->fails()) {
+                        $responce = [
+                            'status'=>'0',
+                            'errors'=>$validator->errors()
+                        ];
+                        return response()->json($responce);
+                    }
+                    $user_id = $request->user_id;
+                    $data['totalInvestment'] = Investment::where('user_id',$user_id)->sum('amount');
+                    $data['numberOfInvestment'] = Investment::where('user_id',$user_id)->count();
+                    $data['totalReturn'] = DB::table('investments as i')
+                    ->join('roi as r', 'i.id', '=', 'r.investment_id')
+                    ->where(['i.user_id'=>$user_id,'r.status'=>'1'])->sum('r.return_amount');
+                }
+                
+                $responce = [
+                    'status'=>'1',
+                    'message'=>"Dashboard Data",
+                    'data'=> $data
+                ];
+            }else{
+                $responce = [
+                    'status'=>'0',
+                    'message'=>"No data Available",
+                ];
+            }
+            return response()->json($responce);
+
+        }
+    
+        public function getNotification(Request $request){
+            $validator = Validator::make($request->all(), [
+                'role'    => 'required|numeric',
+                'user_id' => 'required|numeric',
+                'offset'  => 'required'
+            ]);
+            if ($validator->fails()) {
+                $responce = [
+                    'status'=>'0',
+                    'errors'=>$validator->errors()
+                ];
+                return response()->json($responce);
+            }
+
+            $limit = 10;
+            $offset = 0;
+            $query = DB::table('notifications')->select('*');
+            $query->where(['for_role'=>$request->role,'user_id'=>$request->user_id]);
+            if($request['offset'] > 0 ){
+                $off= $limit * $request['offset'];
+                $query->skip($off);
+            }
+            $query->take($limit);
+            $query->orderBy('id','desc');
+            // Elq();
+            $notification = $query->get();
+            // Plq();
             $responce = [
                 'status'=>'1',
-                'message'=>"Dashboard Data",
-                'data'=> $data
-            ];
-        }else{
-            $responce = [
-                'status'=>'0',
-                'message'=>"No data Available",
-            ];
-        }
-        return response()->json($responce);
-
-    }
-    
-    public function getNotification(Request $request){
-        $validator = Validator::make($request->all(), [
-            'role'    => 'required|numeric',
-            'user_id' => 'required|numeric',
-            'offset'  => 'required'
-        ]);
-        if ($validator->fails()) {
-            $responce = [
-                'status'=>'0',
-                'errors'=>$validator->errors()
+                'message'=>'Notification list',
+                'data'=>$notification,
             ];
             return response()->json($responce);
+            // dd($salesPerson);
         }
-
-        $limit = 10;
-        $offset = 0;
-        $query = DB::table('notifications')->select('*');
-        $query->where(['for_role'=>$request->role,'user_id'=>$request->user_id]);
-        if($request['offset'] > 0 ){
-            $off= $limit * $request['offset'];
-            $query->skip($off);
-        }
-        $query->take($limit);
-        $query->orderBy('id','desc');
-        // Elq();
-        $notification = $query->get();
-        // Plq();
-        $responce = [
-            'status'=>'1',
-            'message'=>'Notification list',
-            'data'=>$notification,
-        ];
-        return response()->json($responce);
-        // dd($salesPerson);
-    }
 }
